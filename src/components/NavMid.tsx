f2bf99a9aad3ab14d5ca1175d6540a44ccf22422
LeonardoKako <fleithkako@gmail.com>
2025-10-07 11:05:27 -0300

    navDesktop done

--- src/components/NavMid.tsx
+++ src/components/NavMid.tsx
@@ -1,10 +1,12 @@
 export function NavMid() {
+  const liStyle =
+    "hover:bg-slate-700 hover:text-slate-300 px-4 py-1 rounded cursor-pointer";
   return (
-    <header className='hidden md:flex bg-amber-400'>
-      <ul className='flex gap-4 p-4'>
-        <li>Home</li>
-        <li>Sobre</li>
-        <li>Contato</li>
+    <header className='hidden md:flex'>
+      <ul className='flex gap-8 p-4 w-full justify-center text-xl italic font-medium text-slate-700'>
+        <li className={liStyle}>Home</li>
+        <li className={liStyle}>Cerimônia</li>
+        <li className={liStyle}>Confirme sua presença</li>
       </ul>
     </header>
   );
