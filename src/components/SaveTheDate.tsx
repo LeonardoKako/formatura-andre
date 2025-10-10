001d309d332e13fe62ff11157b04e137852ac122
LeonardoKako <fleithkako@gmail.com>
2025-10-10 08:54:52 -0300

    changes in

--- src/components/SaveTheDate.tsx
+++ src/components/SaveTheDate.tsx
@@ -4,17 +4,17 @@ export function SaveTheDate() {
   return (
     <section className='flex sm:hidden p-8'>
       <div className='text-center flex flex-col gap-8 w-[65%]'>
-        <h2 className='mt-8 text-2xl md:text-4xl font-medium italic text-emerald-800'>
+        <h2 className='mt-8 text-xl md:text-4xl font-medium italic text-emerald-800'>
           FORMATURA DE MEDICINA
         </h2>
-        <h1 className='text-3xl md:text-5xl font-medium italic text-emerald-950'>
+        <h1 className='text-2xl md:text-5xl font-medium italic text-emerald-950'>
           ANDRÉ RIBEIRO BITTENCOURT
         </h1>
-        <h3 className='text-3xl font-medium italic mt-4 text-emerald-900'>
+        <h3 className='text-2xl font-medium italic mt-4 text-emerald-900'>
           12 . 12 . 2025
         </h3>
       </div>
-      <div className='w-[35%]'>
+      <div className='w-[35%] flex items-center'>
         <img src={fotoAndre} alt='foto andre' />
       </div>
     </section>
