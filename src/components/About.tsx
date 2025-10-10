b27bc309b4f1d11bfae78a2bf41a745e549c34a7
LeonardoKako <fleithkako@gmail.com>
2025-10-10 09:43:17 -0300

    border-top About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,9 @@
 export function About() {
   return (
-    <div className='p-8 pt-0 md:p-12 md:px-30 text-center text-base text-emerald-950 md:text-lg lg:text-xl'>
+    <div
+      className='p-4 pt-4 md:p-12 md:px-30 text-center border-t-4 border-emerald-800 w-[80%]
+    text-emerald-950 md:text-lg lg:text-xl'
+    >
       <p>
         "Queridos amigos e família, minha formatura está chegando e já é motivo
         de imensa alegria. Como decidi investir nesse momento especial por conta
--- src/components/SaveTheDate.tsx
+++ src/components/SaveTheDate.tsx
@@ -2,19 +2,19 @@ import fotoAndre from "../assets/andre-foto-em-pé.png";
 
 export function SaveTheDate() {
   return (
-    <section className='flex sm:hidden p-8'>
-      <div className='text-center flex flex-col gap-8 w-[65%]'>
-        <h2 className='mt-8 text-xl md:text-4xl font-medium italic text-emerald-800'>
+    <section className='flex p-8 justify-between pb-4'>
+      <div className='text-center flex flex-col gap-8 w-[65%] sm:w-[75%] '>
+        <h2 className='mt-8 text-xl sm:text-3xl md:text-4xl font-medium italic text-emerald-800'>
           FORMATURA DE MEDICINA
         </h2>
-        <h1 className='text-2xl md:text-5xl font-medium italic text-emerald-950'>
+        <h1 className='text-2xl sm:text-4xl md:text-5xl font-medium italic text-emerald-950'>
           ANDRÉ RIBEIRO BITTENCOURT
         </h1>
-        <h3 className='text-2xl font-medium italic mt-4 text-emerald-900'>
+        <h3 className='text-2xl sm:text-4xl font-medium italic mt-4 text-emerald-900'>
           12 . 12 . 2025
         </h3>
       </div>
-      <div className='w-[35%] flex items-center'>
+      <div className='w-[35%] flex items-center sm:w-[25%]'>
         <img src={fotoAndre} alt='foto andre' />
       </div>
     </section>
