e910eeedf5b11765919a5c5d689d4eb7ac4208cb
LeonardoKako <fleithkako@gmail.com>
2025-10-13 21:21:13 -0300

    Location responsive

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -10,14 +10,13 @@ export function Location({ title, desc, src, date, hour }: Props) {
   return (
     <div className='w-full p-8 sm:p-12 md:px-20 text-center items-center flex flex-col gap-3'>
       <h1 className='text-3xl text-emerald-700'>{title}</h1>
-      <div className='flex gap-2 items-center'>
+      <div className='flex flex-col gap-1 md:flex-row md:gap-2 items-center'>
         <p className='text-xl text-emerald-950'>{desc}</p>
-        <span>-</span>
+        <span className='hidden md:flex'>-</span>
         <p className='text-xl text-emerald-950'>
           Data: <span className='text-[17px] font-bold'>{date}</span>
         </p>
-        <span>-</span>
-
+        <span className='hidden md:flex'>-</span>
         <p className='text-xl text-emerald-950'>
           Horário: <span className='text-[17px] font-bold'>{hour}</span>
         </p>
